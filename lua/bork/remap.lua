c13500f9aa376ca8d7b45d90b032f69525ac6e6b
Callum Birks <callumbirks@protonmail.com>
2023-10-28 13:35:14 +0100

    Add bind for tmux-sessionizer

--- lua/bork/remap.lua
+++ lua/bork/remap.lua
@@ -1,13 +1,15 @@
 vim.g.mapleader = ' '
 
-vim.keymap.set({ "n", "v", "x" }, "n", "h")
-vim.keymap.set({ "n", "v", "x" }, "e", "j")
-vim.keymap.set({ "n", "v", "x" }, "u", "k")
-vim.keymap.set({ "n", "v", "x" }, "i", "l")
-vim.keymap.set({ "n", "v", "x" }, "h", "n")
-vim.keymap.set({ "n", "v", "x" }, "j", "e")
-vim.keymap.set({ "n", "v", "x" }, "k", "u")
-vim.keymap.set({ "n", "v", "x" }, "l", "i")
+vim.keymap.set({ "n", "v" }, "n", "h")
+vim.keymap.set({ "n", "v" }, "e", "j")
+vim.keymap.set({ "n", "v" }, "u", "k")
+vim.keymap.set({ "n", "v" }, "i", "l")
+vim.keymap.set({ "n", "v" }, "h", "n")
+vim.keymap.set({ "n", "v" }, "j", "e")
+vim.keymap.set({ "n", "v" }, "k", "u")
+vim.keymap.set({ "n", "v" }, "l", "i")
+vim.keymap.set({ "n", "v" }, "L", "I")
+vim.keymap.set("n", "z", "u")
 
 vim.keymap.set("n", "<C-w>n", "<C-w>h")
 vim.keymap.set("n", "<C-w>e", "<C-w>j")
@@ -21,6 +23,8 @@ vim.keymap.set("n", "<C-w><C-i>", "<C-w>l")
 vim.keymap.set("n", "<C-w><C-h>", "<C-w>n")
 
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("t", "<C-space>", "<C-\\><C-n>")
 
 vim.keymap.set("v", "E", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "U", ":m '<-2<CR>gv=gv")
@@ -39,12 +43,13 @@ vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "<leader>d", "\"_d")
-vim.keymap.set("v", "<leader>y", "\"_d")
+vim.keymap.set("v", "<leader>d", "\"_d")
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", function() vim.lsp.buf.format() end)
+vim.keymap.set("n", "<C-f>", "<cmd>!tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<C-u>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-e>", "<cmd>cprev<CR>zz")
