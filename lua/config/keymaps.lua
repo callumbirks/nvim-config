837facffe1c37c8a9e421af64e286ebbfafe5a79
Callum Birks <callumbirks@protonmail.com>
2024-02-21 20:15:34 +0000

    Basic customization

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,21 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Make <C-c> the same as <Esc>
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+-- Yank to end of line
+vim.keymap.set("n", "Y", "yg$")
+
+-- Yank into system clipboard
+vim.keymap.set("n", "<leader>y", '"+y')
+vim.keymap.set("v", "<leader>y", '"+y')
+vim.keymap.set("n", "<leader>Y", '"+Y')
+
+-- Paste without overriding yank buffer
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+-- Delete without overriding yank buffer
+vim.keymap.set("n", "<leader>d", '"_d')
+vim.keymap.set("v", "<leader>d", '"_d')
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,36 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Disable sync with system clipboard
+vim.opt.clipboard = ""
+
+-- Disable LazyVim auto format
+vim.g.autoformat = false
+
+-- Line numbers
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- Indent width 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- More context
+vim.opt.scrolloff = 8
+
+-- No wrap
+vim.opt.wrap = false
+
+-- Using undofile (with undotree)
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- Stop search from being annoying
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
