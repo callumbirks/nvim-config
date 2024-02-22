4e157d8066c483ccfd9ad347065879c07cfeb3c3
Callum Birks <callumbirks@protonmail.com>
2024-02-22 15:52:57 +0000

    Make cmp windows transparent
    Add keymaps for telescope LSP symbol search

--- lua/plugins/overrides.lua
+++ lua/plugins/overrides.lua
@@ -20,4 +20,53 @@ return {
         "L3MON4D3/LuaSnip",
         keys = function() return {} end,
     },
+    -- Add some symbol search keymaps
+    {
+        "nvim-telescope/telescope.nvim",
+        keys = {
+            { "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Find Document Symbols" },
+            { "<leader>fS", "<cmd>Telescope lsp_workspace_symbols<CR>", desc = "Find Workspace Symbols" },
+        },
+    },
+    -- Give noice doc windows a border (better for transparent)
+    {
+        "folke/noice.nvim",
+        opts = {
+            presets = {
+                lsp_doc_border = true,
+            },
+        },
+    },
+    -- Make cmp windows look the same
+    {
+        "hrsh7th/nvim-cmp",
+        opts = function(_, opts)
+            local border = {
+                { "╭", "FloatBorder" },
+                { "─", "FloatBorder" },
+                { "╮", "FloatBorder" },
+                { "│", "FloatBorder" },
+                { "╯", "FloatBorder" },
+                { "─", "FloatBorder" },
+                { "╰", "FloatBorder" },
+                { "│", "FloatBorder" },
+            }
+
+            vim.cmd("hi Pmenu guibg=NONE")
+            vim.cmd("hi Pmenu blend=0")
+            vim.cmd("hi FloatBorder blend=0")
+            opts.window = {
+                completion = {
+                    winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,Search:None",
+                    scrollbar = false,
+                    border = border,
+                },
+                documentation = {
+                    winhighlight = "Normal:Pmenu,CursorLine:PmenuSel,Search:None",
+                    border = border,
+                },
+            }
+            return opts
+        end,
+    },
 }
